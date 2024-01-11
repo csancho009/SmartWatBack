93a9844997cb67e35d5367debb55d7b39d7b8f19
Cristhian Sancho <electronicocr@hotmail.com>
2024-01-10 19:19:33 -0600

    Agregar archivos de proyecto.

---
+++ SmartWatBack/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using SmartWatBack.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SmartWatBack
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
